81f80af62dbc5c34f2a751f33d0bce0f18313723
blanns456 <blannslapening@gmail.com>
2022-11-14 09:37:43 +0800

    first commit

---
+++ gradeGenerator/resources/views/layouts/html.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8" />
+      <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+
+      <title>Grading and Scheduler System</title>
+
+      <base href="./" />
+        <link rel="stylesheet" href="{{asset('public/assets/css/bootstrap.css')}}">
+        <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('public/assets/css/registerpage.css')}}">
+  </head>
+
+  <body>
+    @yield('body')
+    <script src="{{asset('public/assets/popper.js')}}"></script>
+    <script src="{{asset('public/assets/bootstrap.js')}}"></script>
+  </body>
+</html>
